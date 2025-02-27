5251cc091b8e6fcab49616795a756b575c299cb9
test <sandeep.gorre@test.com>
2025-02-27 12:12:42 +0530

    added a new file for testing

---
+++ script.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function ($) {
+    // Event listener for the product download button
+    $('.download-trigger').on('click', function (e) {
+        e.preventDefault();
+    }
+    });
