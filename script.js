3dca1b6607bf3815458bc4d4e3d535f3d80566ec
test <sandeep.gorre@test.com>
2025-03-03 12:30:32 +0530

    merged the script.js file in to the master

---
+++ script.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function ($) {
+    // Event listener for the product download button
+    $('.download-trigger').on('click', function (e) {
+        e.preventDefault();
+    }
+    
+    });
